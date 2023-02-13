24b92a5de12c762f34ec3c97161e4e921a686c8b
Tulgar Dinç <tulgardinc@gmail.com>
2023-02-13 10:25:53 +0100

    based

--- include/shader.h
+++ include/shader.h
@@ -1,7 +1,11 @@
+#pragma once
+
 #ifndef SHADER_H
 #define SHADER_H
 
 #include <glad/glad.h>
+
+#include <glm/glm.hpp>
 
 #include <fstream>
 #include <iostream>
@@ -22,6 +26,7 @@ public:
   void setInt(const std::string &name, int value) const;
   void setFloat(const std::string &name, float value) const;
   void setVec3(const std::string &name, float value[3]) const;
+  void setMat4(const std::string &name, glm::mat4 value) const;
 };
 
 #endif
--- src/tools/shader.cpp
+++ src/tools/shader.cpp
@@ -1,3 +1,6 @@
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <fstream>
 #include <shader.h>
 #include <sstream>
@@ -92,4 +95,9 @@ void Shader::setFloat(const std::string &name, float value) const {
 void Shader::setVec3(const std::string &name, float value[3]) const {
   glUniform3f(glGetUniformLocation(ID, name.c_str()), value[0], value[1],
               value[2]);
+}
+
+void Shader::setMat4(const std::string &name, glm::mat4 value) const {
+  glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE,
+                     glm::value_ptr(value));
 }
